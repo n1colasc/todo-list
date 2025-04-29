2516f789a8a612c3e07b195bde254ccdb5bf8799
Nicolás Castiblanco <nicolascastiblancob@gmail.com>
2025-04-29 14:40:55 -0500

    add settings feature

---
+++ src/app/pages/settings/settings.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RemoteConfigService } from 'src/app/services/remote-config.service';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+  standalone: false,
+})
+export class SettingsPage implements OnInit {
+  valFeature = false;
+  constructor(private remoteConfigService: RemoteConfigService) { }
+
+  async ngOnInit() {
+    await this.handleCheckVal();
+  }
+
+  async handleCheckVal(){
+    this.valFeature = await this.remoteConfigService.isFeatureEnabled("AddTaskIsEnabled");
+  }
+
+}
